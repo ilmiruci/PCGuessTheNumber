a017102b5b6771fe3819ea19f6fdbbbb04c30388
ilmir <adm.eths@gmail.com>
2025-03-04 17:43:05 +0500

    декомпозиция

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-import math
+import utils
 
-print("Введите начало и конец диапазона загаданного числа: ", end="")
-start, end = map(int, input().split())
+def main():
+    print("Введите начало и конец диапазона загаданного числа: ", end="")
+    start, end = utils.get_range()
 
-start, end = min(start, end), max(start, end)
+    utils.validation_range(start, end)
 
-assert start != end, "Диапазон должен быть валидным (разные числа)"
+    max_attempt: int = utils.get_max_attempt(start, end)
+    attempt: int = 1
 
-print("Валидный диапазон")
+    while attempt <= max_attempt:
+        middle: int = (start + end) // 2
 
-max_attempt:int = int(math.log2(end - start)) + 1
-attempt: int = 1
+        print(f"Ответ компьютера: {middle}")
+        ans = utils.get_ans()
+        answers = ("Больше - 1", "Меньше - 2", "Угадал! - 3")
 
-while attempt <= max_attempt:
-    middle: int = (start + end) // 2
+        if ans not in answers:
+            print(f"Возможные ответы: {answers} ")
 
-    print(f"Ответ компьютера: {middle}")
-    ans = int(input("Загаданное число (больше - 1, меньше - 2, угадал - 3): "))
+        if ans == 1:  # больше
+            start = middle + 1
+        elif ans == 2:  # меньше
+            end = middle - 1
+        elif ans == 3:
+            print(f"Я угадал! Загаданное число {middle}. Количество попыток: {attempt}")
+            break
+        attempt += 1
 
-    answers = ("Больше - 1", "Меньше - 2", "Угадал! - 3")
-    if ans not in answers:
-        print(f"Возможные ответы: {answers} ")
 
-    if ans == 1: # больше
-        start = middle + 1
-    elif ans == 2: # меньше
-        end = middle - 1
-    elif ans == 3:
-        print(f"Я угадал! Загаданное число {middle}. Количество попыток: {attempt}")
-        break
-    attempt += 1
-
+if __name__ == "__main__":
+    main()
